f1d9f72ce07bfcc3acd83a85b01697a0f0aab40e
Abe Othman <aothman@cs.cmu.edu>
2014-07-13 20:48:11 -0700

    Get imports working correctly

--- hiscore/__init__.py
+++ hiscore/__init__.py
@@ -4,3 +4,6 @@ __copyright__ = 'Copyright 2014, Abraham Othman'
 __version__ = '1.0'
 __maintainer__ = 'Abraham Othman'
 __email__ = 'aothman@cs.cmu.edu'
+
+from .engine import create, HiScoreEngine
+from .errors import MonotoneError, MonotoneBoundsError, ScoreCreationError
