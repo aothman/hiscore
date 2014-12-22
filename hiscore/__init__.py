bf97ea6e188175cb452fd1534356fc205d1434f7
Abe Othman <aothman@cs.cmu.edu>
2014-12-22 12:04:59 -0400

    Change versioning to be PEP compatible

--- hiscore/__init__.py
+++ hiscore/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Abraham Othman'
 __copyright__ = 'Copyright 2014, Abraham Othman'
 
-__version__ = '1.0'
+__version__ = '1.0.0'
 __maintainer__ = 'Abraham Othman'
 __email__ = 'aothman@cs.cmu.edu'
 
